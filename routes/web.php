437ba5504f2c2075a3bd0ec1c670361b98cdb133
CTM_GoatSan™ <67446286+Go4tSan@users.noreply.github.com>
2021-08-18 20:39:23 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
- Route::get('/', function() { 
+ Route::get('layouts.plantilla', function() { 
      return view('layouts.plantilla');
 });
 Route::get('sobre mi', function () {
